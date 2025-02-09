1b4c04da154cef06748d08e340a6eba67079a951
Katie Ludwig <katierludwig@gmail.com>
2025-02-08 20:29:17 -0800

    completed App.js

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+    <div>
+        <h1>Welcome to LV Nails!</h1>
+        <p>Your beauty is our duty!</p>
+    </div>
+);
+
+export default Home;
