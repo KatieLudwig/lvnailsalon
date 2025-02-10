8b75b3f2637d49233e18dad52227b5d5eb164bdb
Katie Ludwig <katierludwig@gmail.com>
2025-02-10 02:25:33 -0800

    wip week 2 pt 1

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+    <div>
+        <h1>About us!</h1>
+        <p>*about us info here*</p>
+    </div>
+);
+
+export default About; 
--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Services from './Services.js';
+import About from './About';
 import Home from "./Home";
 
 function App() {
@@ -10,7 +12,9 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" exact component={Home} />
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
         </Routes>
         <Footer />
       </div>
