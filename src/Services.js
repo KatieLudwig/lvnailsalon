077f87bb5ec6048ea8213616a8fa2871c33fa903
Katie Ludwig <katierludwig@gmail.com>
2025-02-10 03:22:17 -0800

    service wip week 2 pt 4

--- src/Services.js
+++ src/Services.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
+const services = [
+    {
+        category: 'Maincure Services',
+        items: [
+            { name: 'Polish Chang (Regular)', price: ' & up' },
+            { name: 'One Nail Fix', price: ' & up' },
+            { name: 'Pink & White Fill', price: ' & up' },
+            { name: 'Pink Fill', price: ' & up' },
+            { name: 'Regular Manicure', price: ' & up' },
+            { name: 'Polish Change (Regular)', price: ' & up' },
+            { name: 'Pink & White Ombre Fill', price: ' & up' },
+            { name: 'Gel Set', price: ' & up' },
+            { name: 'Nails Take Off', price: ' & up' },
+            { name: 'Gel Fill', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+            { name: '', price: ' & up' },
+        ]
+    }
+]
 const Services = () => (
     <div>
         <h1>Our Services</h1>
-        <p>Details about services at the nail salon</p>
+        <h2>Pedicure Services: </h2>
+        <p></p>
     </div>
 );
 
