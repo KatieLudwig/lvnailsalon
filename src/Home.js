b5700e229292be61bdc4f57d338210e59df41511
Katie Ludwig <katierludwig@gmail.com>
2025-02-11 12:09:53 -0800

    update styling for home

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
+import './style.css';
 
 const Home = () => (
-    <div>
+    <div className='home-container'>
+        <header className="hero">
         <h1>Welcome to LV Nails!</h1>
-        <p>Your beauty is our duty!</p>
+            <p>Your beauty is our duty!</p>
+        </header>
+        <section className="intro">
+            <h2>About Us</h2>
+            <p>At LV nails, we are commited to providing the best nail care services in town. Our experienced team is here to ensure you leave feeling pampered and beautiful.</p>
+            <button>Discover More</button>
+        </section>
     </div>
 );
 
