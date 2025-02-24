5dcee6f49b323a17eeb99443f5f8b55e2da28a7c
Katie Ludwig <katierludwig@gmail.com>
2025-02-23 16:02:02 -0800

    Update footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Footer = () => (
-    <footer>
+    <footer className="footer">
         <p>Contact us: </p>
         <p>Phone Number: </p>
         <p>Address: 5601 N Tenaya Way Ste 101 Las Vegas, NV 89130</p>
+        <p>&copy: 2025 LV Nails. All rights reserved.</p>
     </footer>
 );
 
