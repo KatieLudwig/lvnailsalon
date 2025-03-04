5a96a1dbe36b6a97a17b9819a4fcf5a674bef8ca
Katie Ludwig <katierludwig@gmail.com>
2025-03-03 19:45:59 -0800

    added second photo

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Gallery = () => (
     <div>
-      <img src="/nail1.jpg" alt="nail1" />
+        <img src="/nail1.jpg" alt="nail1" />
+        <img src="/nail2.jpg" alt="nail2" />
+
       </div>
 );
 
