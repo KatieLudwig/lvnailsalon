5c776438bbb89ed0a0ca760f5302390dcc1c5a96
Katie Ludwig <katierludwig@gmail.com>
2025-03-03 19:16:01 -0800

    wip

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => (
     <div>
         <h1>About us!</h1>
-        <p>*about us info here*</p>
+        <p>We are a local nail salon and we love to help you feel all nice and pampered! We are a family owned buisness, and enjoy making new friends here in Nevada! We would love if you came along our journey and booked with us!</p>
     </div>
 );
 
