490478bd78e58189b5709691e165cf2170d4a18b
Katie Ludwig <katierludwig@gmail.com>
2025-03-03 19:35:42 -0800

    adding photos

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Gallery = () => (
     <div>
-        <h1>put photos here</h1>
-    </div>
+      <img src={`${process.env.PUBLIC_URL}/images/lvnail1.jpg`} alt="My Photo" />
+      </div>
 );
 
 export default Gallery; 
