f9ac0ebb60a79a330dba33dadee305f5bcdd2d7f
Katie Ludwig <katierludwig@gmail.com>
2025-03-09 17:45:26 -0600

    IMPORTED PHOTOS!

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Gallery = () => (
     <div>
-          <img src="./images/nail1.png" alt="Nail 1" className="photo" />
-          <img src="./images/nail2.jpg" alt="Nail 2" className="photo" />
-      </div>
+        <img src="/images/nail1.png" alt="Nail 1" className="photo" />
+        <img src="/images/nail2.jpg" alt="Nail 2" className="photo" />
+
+    </div>
 );
 
-export default Gallery; 
+export default Gallery;
+
