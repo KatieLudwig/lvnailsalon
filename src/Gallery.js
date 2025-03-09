afe6d4e69acd8aa72fd314d451f75e8c4e96b23e
Katie Ludwig <katierludwig@gmail.com>
2025-03-09 17:47:53 -0600

    wip

--- src/Gallery.js
+++ src/Gallery.js
@@ -4,7 +4,7 @@ const Gallery = () => (
     <div>
         <img src="/images/nail1.png" alt="Nail 1" className="photo" />
         <img src="/images/nail2.jpg" alt="Nail 2" className="photo" />
-
+        <img src="/images/nail3.jpg" alt="Nail 3" className='photo' />
     </div>
 );
 
