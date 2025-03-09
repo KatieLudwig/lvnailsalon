0acd2dc9dc24a5b59dcc635f2078243fd4368935
Katie Ludwig <katierludwig@gmail.com>
2025-03-09 17:53:33 -0600

    styling gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Gallery = () => (
-    <div>
+    <div className='gallery-container'>
         <img src="/images/nail1.png" alt="Nail 1" className="photo" />
         <img src="/images/nail2.jpg" alt="Nail 2" className="photo" />
         <img src="/images/nail3.jpg" alt="Nail 3" className='photo' />
