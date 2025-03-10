1056f8892187f321c958276f71c2a1d0053e882d
Katie Ludwig <katierludwig@gmail.com>
2025-03-09 18:15:09 -0600

    adding more photos

--- src/Gallery.js
+++ src/Gallery.js
@@ -5,6 +5,7 @@ const Gallery = () => (
         <img src="/images/nail1.png" alt="Nail 1" className="photo" />
         <img src="/images/nail2.jpg" alt="Nail 2" className="photo" />
         <img src="/images/nail3.jpg" alt="Nail 3" className='photo' />
+        {/* <img src="/images/Nail4.jpg" alt="Nail 4" className='photo' /> */}
     </div>
 );
 
